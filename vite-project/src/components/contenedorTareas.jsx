c847b4e16e2f67115e4886825d872b4468573ba1
DerekNoguera <dereknoguera9@gmail.com>
2024-06-24 16:20:22 -0600

    Todas las carpetas bien estructuradas

---
+++ vite-project/src/components/contenedorTareas.jsx
@@ -0,0 +1,15 @@
+
+function ContenedorTareas() {
+  return (
+    <>
+    <div>
+        <div>
+            <input type="text" />
+            <input type="button" value="" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default ContenedorTareas
---
+++ vite-project/src/components/navTodo.jsx
@@ -0,0 +1,13 @@
+
+function NavTodo() {
+  return (
+    <div>
+      <div className="containerNav"></div>
+      <div className="Grid-txt">
+        <h1 className="tareaTxt">Tareas por hacer </h1>
+        </div>
+    </div>
+  )
+}
+
+export default NavTodo
